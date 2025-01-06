b22fa6aa0a63b76cf0d75516189267eb6fc92082
Franco Galfre <francogalfre2007@gmail.com>
2025-01-06 01:59:16 -0300

    feat: Add Magic UI and ShadCN components

---
+++ src/utils/tipsData.ts
@@ -0,0 +1,28 @@
+export interface Tip {
+  id: number;
+  title: string;
+  description: string;
+}
+
+export const tips: Tip[] = [
+  {
+    id: 1,
+    title: "Tip 1",
+    description: "Description 1",
+  },
+  {
+    id: 2,
+    title: "Tip 2",
+    description: "Description 2",
+  },
+  {
+    id: 3,
+    title: "Tip 3",
+    description: "Description 3",
+  },
+  {
+    id: 4,
+    title: "Tip 4",
+    description: "Description 4",
+  },
+];
