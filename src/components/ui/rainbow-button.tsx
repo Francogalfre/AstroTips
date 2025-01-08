afb3d009b4667688a7fa8e0292da76332c2c8534
Franco Galfre <francogalfre2007@gmail.com>
2025-01-08 20:04:07 -0300

    core: Improve image load performance

--- src/components/ui/rainbow-button.tsx
+++ src/components/ui/rainbow-button.tsx
@@ -7,7 +7,7 @@ export function RainbowButton({ children, className, ...props }: RainbowButtonPr
   return (
     <button
       className={cn(
-        "text-lg group relative inline-flex h-12 animate-rainbow cursor-pointer items-center justify-center rounded-full border-0 bg-[length:200%] px-10 font-medium text-primary-foreground transition-colors [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:scale-105 transition-transform",
+        "text-lg group relative inline-flex h-12 animate-rainbow cursor-pointer items-center justify-center rounded-full border-0 bg-[length:200%] px-10 font-medium text-primary-foreground [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:scale-105 transition-transform",
 
         // dark mode colors
         "text-gray-800 bg-[linear-gradient(#fff,#fff),linear-gradient(#fff_50%,rgba(255,255,255,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))]",
