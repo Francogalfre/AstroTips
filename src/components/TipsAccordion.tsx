b0878e5f9bc9ec32c9f6f7470c2f95e96b4ea5d3
Franco Galfre <francogalfre2007@gmail.com>
2025-01-09 23:54:49 -0300

    style: Finish responsive design in the Tips page

--- src/components/TipsAccordion.tsx
+++ src/components/TipsAccordion.tsx
@@ -13,16 +13,16 @@ const MyAccordion = ({ tips }: { tips: Tip[] }) => {
   const finalTips = !searchParams.get("search") ? tips : filteredTips;
 
   return (
-    <Accordion type="single" collapsible defaultValue={`item-${tips[0].id}`}>
+    <Accordion type="single" collapsible defaultValue={`item-${tips[0].id}`} className="px-2">
       {finalTips.map((tip) => (
         <AccordionItem value={`item-${tip.id}`} key={tip.id} className="border-b border-gray-500 pb-10 pt-10">
           <AccordionTrigger className="text-xl">
-            <Badge variant={"outline"} className="text-gray-300 text-lg font-medium">
+            <Badge variant={"outline"} className="text-gray-300 text-sm sm:text-lg font-medium">
               Tip {tip.id}
             </Badge>
           </AccordionTrigger>
-          <h2 className="text-white text-3xl font-bold">{tip.title}</h2>
-          <AccordionContent className="text-xl prose-2xl text-gray-300 whitespace-pre-line">
+          <h2 className="text-white text-2xl sm:text-3xl font-bold">{tip.title}</h2>
+          <AccordionContent className="text-base sm:text-xl prose-2xl text-gray-300 whitespace-pre-line">
             {tip.content}
           </AccordionContent>
         </AccordionItem>
